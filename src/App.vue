fcbf4dccc61d342ab97d5a75b1dd270eb5ff9283
vino <vino0908@outlook.com>
2021-08-10 21:44:03 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <h1>湖人总冠军</h1>
+  <Vino/>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+import {defineComponent} from "vue";
+import Vino from "./components/Vino.vue";
 
 export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-})
+  name: "App",
+  components: {Vino}
+});
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Vino.vue
@@ -0,0 +1,11 @@
+<template>
+  <h4>Vino</h4>
+</template>
+
+<script lang="ts">
+import {defineComponent} from "vue";
+
+export default defineComponent({
+  name: "Vino"
+});
+</script>
